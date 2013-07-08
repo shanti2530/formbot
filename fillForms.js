e0d3459fe92fb322af899cf6376dfc72659c3672
Maria Camilleri <mariacamilleri@gmail.com>
2013-07-08 11:28:20 +0200

    added email support

--- fillForms.js
+++ fillForms.js
@@ -1,12 +1,17 @@
 javascript:function f(){
     var inputs = document.getElementsByTagName('input');
     
+    var emailRegx = /email/g;
+    
     for(var i = 0; i < inputs.length; i++) {
         var input = inputs[i];
-        if (input.type === 'text') {
-            input.value = Math.random().toString(36).slice(2, 7 + 2);
+        var inputId = input.id;
+        if (input.type === 'email' || emailRegx.test(inputId)) {
+            input.value = 'f@ke.com';
         } else if (input.type === 'password') {
             input.value = 'Password123';
+        } else if (input.type === 'text') {
+            input.value = Math.random().toString(36).slice(2, 7 + 2);   
         }
     }
     
