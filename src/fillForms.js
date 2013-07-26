5223b3fb792f157fe02a3ac13d6a5046943824fe
Maria Camilleri <mariacamilleri@gmail.com>
2013-07-26 20:25:08 +0200

    when trying to identify an element, the label associated is taken into consideration

--- src/fillForms.js
+++ src/fillForms.js
@@ -12,35 +12,44 @@ javascript:function f(){
                 output = output + characters.charAt(Math.random()*54);
             }
             return output;
-        }
+        };
 
         /*A function which given an element checks if it belongs to a certain text string*/
-        var eleContains = function(ele, testString, caseSensitive) {
+        var eleContains = function(ele, testString) {
             var eleId;
             var eleName;
-            if (caseSensitive) {
-                eleId = input.id;
-                eleName = input.name;
+            eleId = input.id.toLowerCase();
+            eleName = input.name.toLowerCase();
+            testString = testString.toLowerCase();
+            if (eleId.indexOf(testString) != -1 || eleName.indexOf(testString) != -1) {
+                return true;
             } else {
-                eleId = input.id.toLowerCase();
-                eleName = input.name.toLowerCase();
-                testString = testString.toLowerCase();
+                var labels = document.getElementsByTagName("label");
+                for (var i = 0; i < labels.length; i++) {
+                    if(labels[i].htmlFor != null && labels[i].htmlFor != ''
+                        && labels[i].htmlFor === input.id){
+                        return (labels[i].innerHTML.toLowerCase().indexOf(testString) != -1)
+                    }
+                }
+                if (ele.parentNode.tagName === 'LABEL') {
+                    return ele.parentNode.innerHTML.toLowerCase().indexOf(testString) != -1;
+                }
             }
-            return (eleId.indexOf(testString) != -1 || eleName.indexOf(testString) != -1);
-        }
+            return false;
+        };
 
 		for(var i = 0; i < inputs.length; i++) {
 			var input = inputs[i];
-
+            console.log(input.value);
             if (input.value != null && input.value.length > 0) {
                 /*we do not alter the value in the text box if it is not empty*/
             } else {
                 if (input.type === 'email'
-                    || eleContains(input, 'mail', false)) {
+                    || eleContains(input, 'mail')) {
                     input.value = 'f@ke.com';
                 } else if (input.type === 'password') {
                     input.value = 'Password123';
-                } else if (eleContains(input, 'card', false)) {
+                } else if (eleContains(input, 'card')) {
                     input.value = '4444333322221111';
                 } else if (input.type === 'text') {
                     input.value = getRandomText(7);
