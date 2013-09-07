3e63b208a2a3811b694bba346e0805818713616c
Maria Camilleri <mariacamilleri@gmail.com>
2013-09-07 19:00:21 +0200

    fill forms bookmarklet overhaul. Mainly seperated functionality so that it should be easier to add new features

--- src/fillForms.js
+++ src/fillForms.js
@@ -1,63 +1,88 @@
 javascript:function f(){
 	
+    var utils = {
+        randomText: 
+            function(len) {
+                var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+                var output = "";
+                for (var i = 0; i < len; i++) {
+                    output = output + characters.charAt(Math.random()*54);
+                }
+                return output;
+            }
+    }
+
     /*The default object with which the fields will be filled up*/
     var defaults = {
-        EMAIL: 'f@ke.com',
-        PASSWORD: 'Password123',
-        CARD_NO: '4444333322221111',
-        CVV: '123',
-        PHONE: '79797979',
-        RANDOM_TEXT: function(len) {
-            var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            var output = "";
-            for (var i = 0; i < len; i++) {
-                output = output + characters.charAt(Math.random()*54);
-            }
-            return output;
+        "EMAIL"    : {value:'f@ke.com'},
+        "PASSWORD" : {value:'Password123'},
+        "CARD_NO"  : {value:'4444333322221111'},
+        "CVV"      : {value:'123'},
+        "PHONE"    : {value:'79797979'},
+        "TEXT"     : {value: utils.randomText(7)}
         }
-    };
+    ;
 
-    /*A function which given an element checks if it belongs to a certain text string*/
-    var eleContains = function(ele, testString) {
-        var eleId = ele.id.toLowerCase();
-        var eleName = ele.name.toLowerCase();
-        testString = testString.toLowerCase();
-        if (eleId.indexOf(testString) != -1 || eleName.indexOf(testString) != -1) {
-            return true;
-        } else {
-            var labels = document.getElementsByTagName("label");
-            for (var i = 0; i < labels.length; i++) {
-                if(labels[i].htmlFor != null && labels[i].htmlFor != ''
-                    && labels[i].htmlFor === input.id){
-                    return (labels[i].innerHTML.toLowerCase().indexOf(testString) != -1)
+    /*Checker text which the inputs should be matched to*/
+    var inputChecker = {
+        defaults: [
+            {includes: ["mail"], type:"EMAIL"},
+            {includes: ["pass"], type:"PASSWORD"},
+            {includes: ["card"], excludes: ["name"], type:"CARD_NO"},
+            {includes: ["cvv"],  type:"CVV"},
+            {includes: ["phone", "tel", "mobile"], type:"PHONE"},
+            {includes: ["text"],  type:"TEXT"},],
+
+
+        //TODO: add excludes functionality
+        checkText: function(text) {
+            for (var i = this.defaults.length - 1; i >= 0; i--) {
+                for (var j = this.defaults[i].includes.length - 1; j >= 0; j--) {
+                    if (text.toLowerCase().indexOf(this.defaults[i].includes[j]) != -1) {
+                        return this.defaults[i].type;
+                    }
                 }
             }
-            if (ele.parentNode.tagName === 'LABEL') {
-                return ele.parentNode.innerHTML.toLowerCase().indexOf(testString) != -1;
+        },
+
+        checkInput: function(input) {
+            
+            var identifiedTextType;
+
+            //check input id
+            var inputId = input.id;
+            if (inputId) {
+                identifiedTextType = this.checkText(inputId);
             }
+
+            if (identifiedTextType && identifiedTextType != undefined) {
+                return identifiedTextType;
+            }    
+            
+            //check input name
+            var inputName = input.name;
+            if (inputName) {
+                identifiedTextType = this.checkText(inputName);
+            }
+            
+            if (identifiedTextType && identifiedTextType != undefined) {
+                return identifiedTextType;
+            }               
+            
+            //check input type
+            var inputType = input.type;
+            if (inputType) {
+                identifiedTextType = this.checkText(inputType);
+            }
+
+            if (identifiedTextType && identifiedTextType != undefined) {
+                return identifiedTextType;
+            }
+
+            //TODO: check input label
+            return identifiedTextType;
         }
-        return false;
     };
-
-    /*A function that given an input tries to guess what type it is and populate its value accordingly*/
-    var guessTextInput = function(element) {
-
-        if (eleContains(element, 'password')) {
-            element.value = defaults.PASSWORD;
-        } else if (eleContains(element, 'mail')) {
-            element.value = defaults.EMAIL;
-        } else if (eleContains(element, 'card')) {
-            element.value = defaults.CARD_NO;
-        } else if (eleContains(element, 'cvv')) {
-            element.value = defaults.CVV;
-        } else if (eleContains(element, 'phone') 
-            || eleContains(element, 'tel')
-            || eleContains(element, 'mobile')) {
-            element.value = defaults.PHONE;
-        } else {
-            element.value = defaults.RANDOM_TEXT(7);
-        }
-    }
 
 	/*A function that given an array of input elements would fill them up 
 	with the respective values*/
@@ -65,17 +90,15 @@ javascript:function f(){
 
 		for(var i = 0; i < inputs.length; i++) {
 			var input = inputs[i];
-			if (input.type === 'checkbox') {
-				/*we tick all checkboxes found*/
+
+            if (input.type === 'checkbox') {
+				/*tick all checkboxes found*/
 				input.checked = true;
-			} else if (input.value != null && input.value.length > 0) {
+			} else if (input.value && input.value.length > 0) {
                 /*we do not alter the value in the text box if it is not empty*/
-            } else if (input.type === 'password') {
-                input.value = defaults.PASSWORD;
-            } else if (input.type === 'email') {
-                input.value = defaults.EMAIL;
-            } else if (input.type === 'text'){
-                guessTextInput(input);
+            } else {
+                var inputCheckerResult = inputChecker.checkInput(input);
+                input.value = defaults[inputCheckerResult].value;
             }
 		}
 	};
