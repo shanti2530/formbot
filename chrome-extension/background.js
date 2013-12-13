e2ec4a5a770064afe77dd78422679937cf354a5d
Maria Camilleri <mariacamilleri@gmail.com>
2013-12-13 17:43:27 +0100

    fixed background to start injecting the script from the chrome folder

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,7 +1,7 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    // code: 'document.body.appendChild(document.createElement("script")).src="//raw.github.com/shanti2530/bookmarklets/master/dest/fillForms.min.js";'
-	code: 'document.body.appendChild(document.createElement("script")).src="../dest/fillForms.min.js";'
+    // code: 'document.body.appendChild(document.createElement("script")).src="//raw.github.com/shanti2530/bookmarklets/master/dest/fillForms.min.js";console.log(chrome.extension.getURL("fillForms.min.js"));'
+	code: 'document.body.appendChild(document.createElement("script")).src=chrome.extension.getURL("fillForms.min.js"); console.log("appended");'
   });
 });
