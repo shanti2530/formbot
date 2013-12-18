ab7774b741c7c6aca4cce3df3973c30f1587ed82
Maria Camilleri <mariacamilleri@gmail.com>
2013-12-18 18:10:19 +0100

    final google analytics commit for bookmarklet

--- src/analytics.js
+++ src/analytics.js
@@ -1,32 +1,7 @@
-// (function() {
-// 	'use strict';
-// 	var ga = document.createElement('script');
-// 	ga.type = 'text/javascript';
-// 	ga.async = true;
-// 	ga.src = 'https://ssl.google-analytics.com/ga.js';
-// 	var s = document.getElementsByTagName('script')[0];
-// 	s.parentNode.insertBefore(ga, s);
-// })();
-
-// var url = '/bookmarklet/' + location.host + location.pathname;
-
-// var _gaq = _gaq || [];
-// _gaq.push(['bookmarkletGA._setAccount', 'UA-44998061-1']);
-// _gaq.push(['bookmarkletGA._setDomainName', 'none']);
-// _gaq.push(['bookmarkletGA._trackPageview', url]);
-// console.log(_gaq);
-
-
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-44998061-1', 'shanti2530.github.io');
-ga('send', 'pageview');
-
-
-// _gaq.push(['_trackEvent', 'bookmarklet', 'clicked']);
-
-// ga('create', 'UA-44998061-1', 'shanti2530.github.io');
-// ga('send', 'pageview');
+ga('send', 'event', 'bookmarklet');
