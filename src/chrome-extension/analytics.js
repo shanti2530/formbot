5112924a9c05095d42ad6830a8d88c37f4ed1b30
Maria Camilleri <mariacamilleri@gmail.com>
2013-12-19 13:20:53 +0100

    updated analytics to start sending chrome-extension event if it is being triggered from the chrome extension

--- src/chrome-extension/analytics.js
+++ src/chrome-extension/analytics.js
@@ -1,3 +1,2 @@
-
 ga('create', 'UA-44998061-1', 'shanti2530.github.io', {'name': 'formFiller'});
-ga('formFiller.send', 'event', 'bookmarklet', location.host);
+ga('formFiller.send', 'event', 'chrome-extension', location.host);
