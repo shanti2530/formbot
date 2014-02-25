0d7b2a2b2a4aecba7192ad79447265ed94b78c51
Maria Camilleri <mariacamilleri@gmail.com>
2014-02-25 10:55:30 +0100

    Bug #28: Trigger change event after pre-filling input

--- src/chrome-extension/defaultvalues.js
+++ src/chrome-extension/defaultvalues.js
@@ -1,6 +1,15 @@
 var fillInput = function(input, data) {
 	'use strict';
     input.value = data;
+
+    //trigger change event after the value is updated
+    if ('createEvent' in document) {
+        var evt = document.createEvent('HTMLEvents');
+        evt.initEvent('change', true, false);
+        input.dispatchEvent(evt);
+    } else {
+        input.fireEvent('onchange');
+    }
 };
 
 var getDefaultValue = function (inputType, maxLength, input) {
