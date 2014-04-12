38958916ee3db2f820b98b6ed2474a997249dc1f
Maria Camilleri <mariacamilleri@gmail.com>
2014-04-12 09:02:11 +0200

    Added array indexof utility function

--- src/utils.js
+++ src/utils.js
@@ -20,5 +20,24 @@ var utils = {
             if (typeof moment === 'function') {
                 return moment().format(format);
             }
+        },
+    indexOf: 
+        function (needle) {
+            if(typeof Array.prototype.indexOf === 'function') {
+                indexOf = Array.prototype.indexOf;
+            } else {
+                indexOf = function(needle) {
+                    var i = -1, index = -1;
+
+                    for(i = 0; i < this.length; i++) {
+                        if(this[i] === needle) {
+                            index = i;
+                            break;
+                        }
+                    }
+                    return index;
+                };
+            }
+        return indexOf.call(this, needle);
         }
 };
