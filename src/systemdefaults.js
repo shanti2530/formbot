d3ce550620c4eb85dc0299f49e95171583911fcb
Maria Camilleri <mariacamilleri@gmail.com>
2014-04-12 09:25:02 +0200

    changed system defaults structure

--- src/systemdefaults.js
+++ src/systemdefaults.js
@@ -2,23 +2,23 @@
 var getDefaults = function() {
     'use strict';
 
-    var defaults = {
-        'EMAIL'    : {defaultValue:'f@ke.com', includes: ['mail']},
-        'PASSWORD' : {defaultValue:'Password123', includes: ['pass']},
-        'CARD_NO'  : {defaultValue:'4444333322221111', includes: ['card'], excludes: ['name', 'code']},
-        'CVV'      : {defaultValue:'123', includes: ['cvv']},
-        'PHONE'    : {defaultValue:'79797979', includes: ['phone', 'tel', 'mobile']},
-        'USERNAME' : {defaultValue: 'john', includes: ['username', 'userId']},
-        'DOMAIN'   : {defaultValue: 'fakeaddresshere.com', includes: ['domain']},
-        'URL'      : {defaultValue: 'http://www.fakeaddresshere.com', includes: ['url', 'site']},
-        'NUMBER'   : {defaultValue: 5, includes: ['number', 'amount', 'range']},
-        'DATETIME' : {defaultValue: utils.getDateFormat('YYYY-MM-DDTHH:mm'), includes: ['datetime']},
-        'DATE'     : {defaultValue: utils.getDateFormat('YYYY-MM-DD'), includes: ['date']},
-        'TIME'     : {defaultValue: utils.getDateFormat('HH:mm'), includes: ['time']},
-        'WEEK'     : {defaultValue: utils.getDateFormat('GGGG-[W]WW'), includes: ['week']},
-        'MONTH'    : {defaultValue: utils.getDateFormat('YYYY-MM'), includes: ['month']},
-        'TEXT'     : {defaultValue: 'Lorem', includes: ['text']}
-    };
+    var defaults = [
+        {name:'EMAIL',    value: {defaultValue:'f@ke.com', includes: ['mail']}},
+        {name:'PASSWORD', value: {defaultValue:'Password123', includes: ['pass']}},
+        {name:'CARD_NO',  value: {defaultValue:'4444333322221111', includes: ['card'], excludes: ['name', 'code']}},
+        {name:'CVV',      value:{defaultValue:'123', includes: ['cvv']}},
+        {name:'PHONE',    value:{defaultValue:'79797979', includes: ['phone', 'tel', 'mobile']}},
+        {name:'USERNAME', value:{defaultValue: 'john', includes: ['username', 'userId']}},
+        {name:'DOMAIN',   value:{defaultValue: 'fakeaddresshere.com', includes: ['domain']}},
+        {name:'URL',      value:{defaultValue: 'http://www.fakeaddresshere.com', includes: ['url', 'site']}},
+        {name:'NUMBER',   value: {defaultValue: 5, includes: ['number', 'amount', 'range']}},
+        {name:'DATETIME', value: {defaultValue: utils.getDateFormat('YYYY-MM-DDTHH:mm'), includes: ['datetime']}},
+        {name:'DATE',     value: {defaultValue: utils.getDateFormat('YYYY-MM-DD'), includes: ['date']}},
+        {name:'TIME',     value: {defaultValue: utils.getDateFormat('HH:mm'), includes: ['time']}},
+        {name:'WEEK',     value: {defaultValue: utils.getDateFormat('GGGG-[W]WW'), includes: ['week']}},
+        {name:'MONTH',    value: {defaultValue: utils.getDateFormat('YYYY-MM'), includes: ['month']}},
+        {name:'TEXT',     value: {defaultValue: 'Lorem', includes: ['text']}}
+    ];
     return defaults;
 };
 
