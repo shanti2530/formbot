4a2b48c026060198394e8f3ada1e93e49fb9d84c
Maria Camilleri <mariacamilleri@gmail.com>
2014-07-08 23:23:06 +0200

    Updated input boxes

--- src/chrome-extension/options.js
+++ src/chrome-extension/options.js
@@ -1,53 +1,59 @@
 window.onload = function() {
 	'use strict';
 
-	//all text inputs (which in this case are all inputs except radio buttons)
-//	Array.prototype.slice.call(document.querySelectorAll('input:not([type=radio])')).forEach(function(el) {
-//
-//		var elementName = el.name.toUpperCase();
-//
-//		//fill the inputs with the default values already stored in the local storage
-//		if(localStorage[elementName]) {
-//			var elementDetails = JSON.parse(localStorage[elementName]);
-//			el.value = elementDetails.defaultValue;
-//		}
-//
-//		//save all inputs in chrome's local storage upon change
-//		el.onchange = function(event) {
-//
-//			//get the element and its data
-//			var element = event.srcElement;
-//
-//			var elementValue = element.value;
-//
-//			if(localStorage[elementName]) {
-//				var elementLocalStorage = JSON.parse(localStorage[elementName]);
-//
-//				//save the value in the input to the local storage
-//				localStorage[elementName] = JSON.stringify({unique: elementLocalStorage.unique,
-//															defaultValue: elementValue,
-//															uniqueValue: elementLocalStorage.uniqueValue});
-//			}
-//		};
-//	});
+	//all text inputs (which in this case are all inputs except checkboxes)
+	Array.prototype.slice.call(document.querySelectorAll('input:not([type=checkbox])')).forEach(function(el) {
 
+		var elementName = el.name.toUpperCase();
+
+		//fill the inputs with the default values already stored in the local storage
+		if(localStorage[elementName]) {
+			var elementDetails = JSON.parse(localStorage[elementName]);
+			el.value = elementDetails.defaultValue;
+
+      //if the field is set to randomly generate .. then disable the input for defined field
+      if (elementDetails.unique) {
+        el.disabled = true;
+      }
+		}
+
+		//save all inputs in chrome's local storage upon change
+		el.onchange = function(event) {
+
+			//get the element and its data
+			var element = event.srcElement;
+
+			var elementValue = element.value;
+
+			if(localStorage[elementName]) {
+				var elementLocalStorage = JSON.parse(localStorage[elementName]);
+
+				//save the value in the input to the local storage
+				localStorage[elementName] = JSON.stringify({unique: elementLocalStorage.unique,
+															defaultValue: elementValue,
+															uniqueValue: elementLocalStorage.uniqueValue});
+			}
+		};
+	});
+
+  //get the checkboxes which define if the field should be random or defined
   Array.prototype.slice.call(document.querySelectorAll('input[type=checkbox][name=onoffswitch]')).forEach(function(el) {
 
     var elementType = el.id.toUpperCase();
     var type = JSON.parse(localStorage[elementType]);
-    el.checked = type.unique;
+    el.checked = !type.unique;
 
-
+    //every time it changes ..
     el.onclick = function (event) {
 
       var element = event.srcElement;
 
       var elementType = element.id.toUpperCase();
-      console.log(elementType);
       var type = JSON.parse(localStorage[elementType]);
 
+      element.checked = type.unique;
 
-      element.checked = !type.unique;
+      document.getElementsByName(el.id)[0].disabled = !type.unique;
 
       localStorage[elementType] = JSON.stringify({unique: !type.unique,
 														  defaultValue: type.defaultValue,
