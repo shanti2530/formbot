14b62967a7fdd7fb31033db4d3eee0bf603aa777
Maria Camilleri <mariacamilleri@gmail.com>
2014-07-12 13:59:14 +0200

    Background refactoring

--- src/chrome-extension/background.js
+++ src/chrome-extension/background.js
@@ -8,19 +8,35 @@ chrome.browserAction.onClicked.addListener(function() {
   'use strict';
   /* jshint ignore:start */
   include "utils.js"
-  include "systemdefaults.js"
   /* jshint ignore:end */
 
   //try to get the input value from the user defined values
-  var defaults = getDefaults();
-  for (var d in defaults) {
+  var defaults = [
+    {name:'EMAIL',    value: {defaultValue:'f@ke.com', includes: ['mail']}},
+    {name:'PASSWORD', value: {defaultValue:'Password123', includes: ['pass']}},
+    {name:'CARD_NO',  value: {defaultValue:'4444333322221111', includes: ['card'], excludes: ['name', 'code']}},
+    {name:'CVV',      value:{defaultValue:'123', includes: ['cvv', 'cvc', 'cv2']}},
+    {name:'PHONE',    value:{defaultValue:'79797979', includes: ['phone', 'tel', 'mobile']}},
+    {name:'USERNAME', value:{defaultValue: 'john', includes: ['username', 'userId']}},
+    {name:'DOMAIN',   value:{defaultValue: 'fakeaddresshere.com', includes: ['domain']}},
+    {name:'URL',      value:{defaultValue: 'http://www.fakeaddresshere.com', includes: ['url', 'site']}},
+    {name:'NUMBER',   value: {defaultValue: 5, includes: ['number', 'amount', 'range']}},
+    {name:'DATETIME', value: {defaultValue: utils.getDateFormat('YYYY-MM-DDTHH:mm'), includes: ['datetime']}},
+    {name:'DATE',     value: {defaultValue: utils.getDateFormat('YYYY-MM-DD'), includes: ['date']}},
+    {name:'TIME',     value: {defaultValue: utils.getDateFormat('HH:mm'), includes: ['time']}},
+    {name:'WEEK',     value: {defaultValue: utils.getDateFormat('GGGG-[W]WW'), includes: ['week']}},
+    {name:'MONTH',    value: {defaultValue: utils.getDateFormat('YYYY-MM'), includes: ['month']}},
+    {name:'TEXT',     value: {defaultValue: 'Lorem', includes: ['text']}}
+  ];
+
+  for (var d=0; d < defaults.length; d++) {
     var type = defaults[d].name;
     var val = localStorage[type];
 
     //if not found in the local storage then set the system default value
     if (val === undefined) {
       localStorage[type] = JSON.stringify({unique: false,
-        defaultValue: getSystemDefault(type, 7),
+        defaultValue: defaults[d].value.defaultValue,
         uniqueValue: 'getUniqueValue'});
     }
   }
@@ -44,8 +60,42 @@ chrome.runtime.onMessage.addListener(
 
     /* jshint ignore:start */
     include "utils.js"
-    include "systemdefaults.js"
     /* jshint ignore:end */
+
+    var getUniqueValue = function(inputType) {
+      switch (inputType) {
+        case 'TEXT':
+          return chance.word({length: 7});
+        case 'DOMAIN':
+          return chance.domain();
+        case 'EMAIL':
+          return chance.email();
+        case 'PASSWORD':
+          return chance.string({length: 7});
+        case 'CARD_NO':
+          return chance.cc();
+        case 'CVV':
+          return chance.integer({min:100, max:999});
+        case 'PHONE':
+          return chance.phone();
+        case 'USERNAME':
+          return chance.word({length: 7});
+        case 'URL':
+          return 'http://' + chance.domain();
+        case 'NUMBER':
+          return chance.integer({min:1,max:99});
+        case 'DATETIME':
+          return utils.getDateFormat('YYYY-MM-DDTHH:mm');
+        case 'DATE':
+          return utils.getDateFormat('YYYY-MM-DD');
+        case 'TIME':
+          return utils.getDateFormat('HH:mm');
+        case 'WEEK':
+          return utils.getDateFormat('GGGG-[W]WW');
+        case 'MONTH':
+          return utils.getDateFormat('YYYY-MM');
+      }
+    };
 
     if (request.method === 'getInputValue') {
 
