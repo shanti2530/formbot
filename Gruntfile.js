2cae7cc23587fc67a974c7bf98943e44ec3e9336
Maria Camilleri <mariacamilleri@gmail.com>
2014-10-09 16:45:47 +0200

    Options page reworked

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,15 +15,11 @@ module.exports = function(grunt) {
     copy: {
       chromeextension: {
         files: [
-//          {src: 'bower_components/angular/angular.min.js', dest: 'src/chrome-extension/', flatten: true, expand: true, filter: 'isFile'},
-//          {src: 'bower_components/angular-toggle-switch/angular-toggle-switch.min.js', dest: 'src/chrome-extension/', flatten: true, expand: true, filter: 'isFile'},
-//          {src: 'bower_components/angular-toggle-switch/*.css', dest: 'src/chrome-extension/', flatten: true, expand: true, filter: 'isFile'},
           {src: 'src/*', dest: 'gen/chrome-extension/', flatten: true, expand:true, filter: 'isFile'},
-//          {src: 'src/chrome-extension/angular-toggle-switch-bootstrap.css', dest: 'gen/chrome-extension/', flatten: true, expand:true, filter: 'isFile'},
-//          {src: 'src/chrome-extension/angular-toggle-switch.css', dest: 'gen/chrome-extension/', flatten: true, expand:true, filter: 'isFile'},
           {src: 'src/chrome-extension/*.js', dest: 'gen/chrome-extension/', flatten: true, expand:true, filter: 'isFile'},
           {src: 'src/chrome-extension/*.json', dest: 'dist/chrome-extension/', flatten: true, expand:true, filter: 'isFile'},
-          {src: 'src/chrome-extension/*.css', dest: 'dist/chrome-extension/', flatten: true, expand:true, filter: 'isFile'}
+          {src: 'src/chrome-extension/*.css', dest: 'dist/chrome-extension/', flatten: true, expand:true, filter: 'isFile'},
+          {src: 'src/chrome-extension/*.png', dest: 'dist/chrome-extension/', flatten: true, expand:true, filter: 'isFile'}
         ]
       },
       chromeextensiondist: {
@@ -35,8 +31,6 @@ module.exports = function(grunt) {
           {src: 'gen/chrome-extension/options.js', dest: 'dist/chrome-extension/options.js'},
           {src: 'gen/chrome-extension/optionsController.js', dest: 'dist/chrome-extension/optionsController.js'},
           {src: 'bower_components/angular/angular.min.js', dest: 'dist/chrome-extension/angular.min.js'},
-//          {src: 'gen/chrome-extension/angular-toggle-switch.min.js', dest: 'dist/chrome-extension/angular-toggle-switch.min.js'},
-//          {src: 'gen/chrome-extension/angular-toggle-switch/*.css', dest: 'dist/chrome-extension/'},
           {src: 'bower_components/momentjs/min/moment.min.js', dest: 'dist/chrome-extension/scripts/moment.min.js'},
           {src: 'node_modules/chance/chance.js', dest: 'dist/chrome-extension/scripts/chance.js'}
         ]
