191c85a30bc7a35d87d56c973b4dfeabd18e14c0
Maria Camilleri <mariacamilleri@gmail.com>
2014-10-26 10:15:56 +0100

    Fixed css bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,6 +34,7 @@ module.exports = function(grunt) {
           {src: 'bower_components/angular/angular.min.js', dest: 'dist/chrome-extension/angular.min.js'},
           {src: 'bower_components/angular/angular.min.js.map', dest: 'dist/chrome-extension/angular.min.js.map'},
           {src: 'bower_components/momentjs/min/moment.min.js', dest: 'dist/chrome-extension/scripts/moment.min.js'},
+          {src: 'bower_components/bootstrap/dist/css/bootstrap.min.css', dest: 'dist/chrome-extension/bootstrap.min.css'},
           {src: 'node_modules/chance/chance.js', dest: 'dist/chrome-extension/scripts/chance.js'}
         ]
       }
