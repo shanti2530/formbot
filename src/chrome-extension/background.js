a570f88e57285eacfc95aed5548491b677ba5891
Maria Camilleri <mariacamilleri@gmail.com>
2014-11-22 13:07:14 +0100

    Addition of user voice and typekit fonts

--- src/chrome-extension/background.js
+++ src/chrome-extension/background.js
@@ -235,3 +235,25 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+  var requestHeaders = details.requestHeaders;
+  for (var i=0; i<requestHeaders.length; ++i) {
+    if (requestHeaders[i].name.toLowerCase() === 'referer') {
+      // The request was certainly not initiated by a Chrome extension...
+      return;
+    }
+  }
+  // Set Referer
+  requestHeaders.push({
+    name: 'referer',
+    // Host must match the domain in your Typekit kit settings
+    value: 'https://fekpdndhlbaagbhmlfhahnknjjcdjppd/'
+  });
+  return {
+    requestHeaders: requestHeaders
+  };
+}, {
+  urls: ['*://use.typekit.net/*'],
+  types: ['stylesheet']
+}, ['requestHeaders','blocking']);
