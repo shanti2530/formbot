dcd87961df0f7b383203317768ab757fef654aa2
Maria Camilleri <mariacamilleri@gmail.com>
2014-11-22 14:20:53 +0100

    Removing unnecssary utils

--- src/utils.js
+++ src/utils.js
@@ -1,43 +1,23 @@
 var utils = {
-    randomText:
-        function(len) {
-          'use strict';
-          return chance.word({length: len});
-        },
-    randomNumber:
-        function(min, max) {
-			'use strict';
-            return chance.integer(min, max);
-        },
-    getDateFormat:
-        function(format) {
-			'use strict';
-            if (typeof moment === 'function') {
-                return moment().format(format);
-            }
-        },
-    isEmpty: function(variable) {
-        'use strict';
-        if (!variable || variable === '' || variable === 'undefined') {
-            return true;
-        }
-    },
-    isObjectEmpty: function(obj) {
+  getDateFormat:
+    function(format) {
       'use strict';
-      return Object.keys(obj).length === 0;
+      if (typeof moment === 'function') {
+        return moment().format(format);
+      }
     },
-    contains:
-        function (array, needle) {
-            'use strict';
-            if (!array || array === undefined || array.length === 0) {
-                return false;
-            }
+  contains:
+    function (array, needle) {
+      'use strict';
+      if (!array || array === undefined || array.length === 0) {
+        return false;
+      }
 
-            for(var i = 0; i < array.length; i++) {
-                if(needle.toLowerCase().indexOf(array[i]) > -1) {
-                    return true;
-                }
-            }
-            return false;
+      for(var i = 0; i < array.length; i++) {
+        if(needle.toLowerCase().indexOf(array[i]) > -1) {
+          return true;
         }
+      }
+      return false;
+    }
 };
